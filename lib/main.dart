ff458a194d36b2962b1e9e888c948f771738f028
Kenta Wakasa <wakasa.kenta.pg@gmail.com>
2023-01-13 12:45:32 +0900

    firebase analytic の導入

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -11,7 +12,9 @@ void main() async {
   usePathUrlStrategy();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
+  await FirebaseAnalytics.instance.logEvent(
+    name: 'app_start',
+  );
   runApp(const MyApp());
 }
 
